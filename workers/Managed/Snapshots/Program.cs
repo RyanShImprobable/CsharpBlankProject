136fb5d35510d30a4caf4f3492e5f7ed9fab34c9
RyanShImprobable <liangren@improbable.io>
2020-04-03 18:21:15 +0800

    Add snapshots

---
+++ workers/Managed/Snapshots/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Improbable.Worker;
+using Environment = Managed.Environment;
+
+namespace Snapshots
+{
+    class Snapshots
+    {
+        private static long id = 1L;
+        private static EntityId NextId
+        {
+            get { return new EntityId(id++); }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static void GenerateBasic(string path, int numAnts, int WorldSize, int FoodClumps)
+        {
+            IDictionary<EntityId, Entity> entities = new Dictionary<EntityId, Entity>();
+
+        }
+    }
+}
